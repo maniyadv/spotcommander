4de04a09d72eacfbabd72b2c39eeb729beaf7366
Ole Jon Bjørkum <mail@olejon.fake>
2013-12-15 18:58:10 +0100

    SpotCommander 8.9

---
+++ main.php
@@ -0,0 +1,149 @@
+<?php
+
+/*
+
+Copyright 2013 Ole Jon Bjørkum
+
+This file is part of SpotCommander.
+
+SpotCommander is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SpotCommander is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SpotCommander.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Project
+
+define('project_name', 'SpotCommander');
+define('project_version', 8.9);
+define('project_serial', 1268);
+define('project_website', 'http://www.olejon.net/code/spotcommander/');
+define('project_developer', 'Ole Jon Bjørkum');
+define('project_android_app_minimum_version', 3.2);
+
+// Configuration
+
+require_once('config.php');
+
+// Functions
+
+require_once('functions.php');
+
+// Daemon
+
+define('daemon_socket', '/tmp/spotcommander-' . daemon_user() . '.socket');
+
+// Remote control
+
+if(isset($_POST['action']))
+{
+	$action = $_POST['action'];
+	$data = (isset($_POST['data'])) ? $_POST['data'] : '';
+
+	if($action == 'launch_quit')
+	{
+		$action = (spotify_is_running()) ? 'spotify_quit' : 'spotify_launch';
+		clear_queue();
+		remote_control($action, $data);
+	}
+	elseif($action == 'play_pause' || $action == 'pause')
+	{
+		remote_control($action, $data);
+	}
+	elseif($action == 'previous' || $action == 'next')
+	{
+		remote_control($action, $data);
+		if(!queue_is_empty()) echo 'queue_is_empty';
+	}
+	elseif($action == 'toggle_shuffle' || $action == 'toggle_repeat')
+	{
+		remote_control($action, $data);
+		if(!spotify_is_running()) echo 'spotify_is_not_running';
+	}
+	elseif($action == 'adjust_spotify_volume' || $action == 'adjust_system_volume')
+	{
+		$volume = $data;
+		$current_volume = get_current_volume();
+
+		if(is_numeric($volume))
+		{
+			$volume = intval($volume);
+			if($volume == 0) set_volume_before_mute($current_volume);
+		}
+		elseif($volume == 'mute')
+		{
+			if($current_volume == 0)
+			{
+				$volume = intval(get_volume_before_mute());
+			}
+			else
+			{
+				$volume = 0;
+				set_volume_before_mute($current_volume);
+			}
+		}
+		elseif($volume == 'down')
+		{
+			$volume = intval($current_volume - 10);
+		}
+		elseif($volume == 'up')
+		{
+			$volume = intval($current_volume + 10);
+		}
+
+		$volume = (spotify_is_running() && is_int($volume)) ? $volume : 50;
+
+		if($volume < 0)
+		{
+			$volume = 0;
+		}
+		elseif($volume > 100)
+		{
+			$volume = 100;
+		}
+
+		remote_control($action, $volume);
+
+		echo $volume;
+	}
+	elseif($action == 'play_uri' || $action == 'play_uri_randomly' || $action == 'start_track_radio')
+	{
+		clear_queue();
+		remote_control($action, $data);
+	}
+	elseif($action == 'get_cover_art')
+	{
+		echo get_cover_art($data);
+	}
+	elseif($action == 'native_app_action')
+	{
+		echo native_app_action($data);
+	}
+	elseif($action == 'clear_cache')
+	{
+		clear_cache();
+	}
+}
+elseif(isset($_GET['global_variables']))
+{
+	echo json_encode(array(project_name, project_version, project_serial, project_website, project_developer, project_android_app_minimum_version, check_for_errors()));
+}
+elseif(isset($_GET['hostname']))
+{
+	echo php_uname('n');
+}
+elseif(isset($_GET['check_for_updates']))
+{
+	echo check_for_updates();
+}
+
+?>
---
+++ recently-played.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+
+Copyright 2013 Ole Jon Bjørkum
+
+This file is part of SpotCommander.
+
+SpotCommander is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SpotCommander is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SpotCommander.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+require_once('main.php');
+
+if(isset($_GET['clear']))
+{
+	echo clear_recently_played();
+}
+else
+{
+	$activity = array();
+	$activity['title'] = 'Recently played';
+	$activity['actions'][] = array('action' => array('Clear', 'remove_32_img_div'), 'keys' => array('actions'), 'values' => array('clear_recently_played'));
+
+	$tracks = get_db_rows('recently-played', "SELECT artist, title, uri FROM recently_played ORDER BY id DESC", array('artist', 'title', 'uri'));
+
+	if(empty($tracks))
+	{
+		echo '
+			<div id="activity_inner_div" data-activitydata="' . base64_encode(json_encode($activity)) . '">
+
+			<div id="activity_message_div"><div><div class="img_div img_64_div information_64_img_div"></div></div><div>No recently played tracks</div></div>
+
+			</div>
+		';
+	}
+	else
+	{
+		echo '
+			<div id="activity_inner_div" data-activitydata="' . base64_encode(json_encode($activity)) . '">
+
+			<div class="list_header_div"><div><div>ALL</div></div><div></div></div>
+
+			<div class="list_div">
+		';
+
+		foreach($tracks as $track)
+		{
+			$artist = $track['artist'];
+			$title = $track['title'];
+			$uri = $track['uri'];
+
+			$actions_dialog = array();
+			$actions_dialog['title'] = hsc($title);
+			$actions_dialog['actions'][] = array('text' => 'Browse album', 'keys' => array('actions', 'uri'), 'values' => array('hide_dialog browse_album', $uri));
+			$actions_dialog['actions'][] = array('text' => 'Browse artist', 'keys' => array('actions', 'uri'), 'values' => array('hide_dialog browse_artist', $uri));
+			$actions_dialog['actions'][] = array('text' => 'Start track radio', 'keys' => array('actions', 'uri', 'playfirst'), 'values' => array('hide_dialog start_track_radio', $uri, 'true'));
+			$actions_dialog['actions'][] = array('text' => 'Lyrics', 'keys' => array('actions', 'activity', 'subactivity', 'args'), 'values' => array('hide_dialog change_activity', 'lyrics', '', 'artist=' . rawurlencode($artist) . '&amp;title=' . rawurlencode($title)));
+			$actions_dialog['actions'][] = array('text' => 'Share', 'keys' => array('actions', 'title', 'uri'), 'values' => array('hide_dialog share_uri', hsc($title), rawurlencode(uri_to_url($uri))));
+
+			echo '
+				<div class="list_item_div">
+				<div title="' . hsc($artist . ' - ' . $title) . '" class="list_item_main_div actions_div" data-actions="toggle_list_item_actions" data-highlightotherelement="div.list_item_main_corner_arrow_div" data-highlightotherelementparent="div.list_item_div" data-highlightotherelementclass="corner_arrow_dark_grey_highlight" onclick="void(0)">
+				<div class="list_item_main_actions_arrow_div"></div>
+				<div class="list_item_main_corner_arrow_div"></div>
+				<div class="list_item_main_inner_div">
+				<div class="list_item_main_inner_icon_div"><div class="img_div img_24_div ' . track_is_playing($uri, 'icon') . '"></div></div>
+				<div class="list_item_main_inner_text_div"><div class="list_item_main_inner_text_upper_div ' . track_is_playing($uri, 'text') . '">' . hsc($title) . '</div><div class="list_item_main_inner_text_lower_div">' . hsc($artist) . '</div></div>
+				</div>
+				</div>
+				<div class="list_item_actions_div">
+				<div class="list_item_actions_inner_div">
+				<div title="Play" class="actions_div" data-actions="play_uri" data-uri="' . $uri . '" data-highlightclass="dark_grey_highlight" data-highlightotherelement="div.list_item_main_actions_arrow_div" data-highlightotherelementparent="div.list_item_div" data-highlightotherelementclass="up_arrow_dark_grey_highlight" onclick="void(0)"><div class="img_div img_24_div play_24_img_div"></div></div>
+				<div title="Queue" class="actions_div" data-actions="queue_uri" data-artist="' . rawurlencode($artist) . '" data-title="' . rawurlencode($title) . '" data-uri="' . $uri . '" data-highlightclass="dark_grey_highlight" onclick="void(0)"><div class="img_div img_24_div queue_24_img_div"></div></div>
+				<div title="Star" class="actions_div" data-actions="star_uri" data-type="track" data-artist="' . rawurlencode($artist) . '" data-title="' . rawurlencode($title) . '" data-uri="' . $uri . '" data-highlightclass="dark_grey_highlight" onclick="void(0)"><div class="img_div img_24_div ' . uri_is_starred($uri) . '_24_img_div"></div></div>
+				<div title="More by ' . hsc($artist) . '" class="actions_div" data-actions="search_spotify" data-string="' . rawurlencode('artist:"' . $artist . '"') . '" data-highlightclass="dark_grey_highlight" onclick="void(0)"><div class="img_div img_24_div search_24_img_div"></div></div>
+				<div title="More" class="actions_div" data-actions="show_actions_dialog" data-dialogactions="' . base64_encode(json_encode($actions_dialog)) . '" data-highlightclass="dark_grey_highlight" onclick="void(0)"><div class="img_div img_24_div overflow_24_img_div"></div></div>
+				</div>
+				</div>
+				</div>
+			';
+		}
+
+		echo '</div></div>';
+	}
+}
+
+?>
